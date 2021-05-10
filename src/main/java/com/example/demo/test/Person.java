8a3f568ee805ffa100d82c4b1fc945ab8932eada
zhouquanbo <55747115+zhouquanbo@users.noreply.github.com>
2021-05-10 11:17:33 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/test/Person.java
@@ -0,0 +1,18 @@
+package com.example.demo.test;
+
+/**
+ * @author pangHu
+ * @date 2021/5/10 11:15
+ */
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
